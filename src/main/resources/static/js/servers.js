ab8fa8e80987f9b4501762fb110a6baf6d2236a5
Amine <Amine@192.168.1.56>
2018-03-11 23:53:00 +0100

    add server

--- src/main/resources/static/js/servers.js
+++ src/main/resources/static/js/servers.js
@@ -1,10 +1,69 @@
 $(function() {
 
-    initServersDataTables();
+	initServersDataTables();
+	initModal();
+	
+	function initServersDataTables() {
+		var serversDataTable = $('#servers-datatables').DataTable(
+				dataTablesConfig);
+		return serversDataTable.rows().count();
+	}
 
-    function initServersDataTables() {
-	var serversDataTable = $('#servers-datatables').DataTable(dataTablesConfig);
-	return serversDataTable.rows().count();
-    }
+	function initModal() {
 
+		var modal = $('#page-wrapper').find('#addServerModal');
+		var actionForm = $(modal).find('form');
+		var closeBtn = $(modal).find('.close');
+		var backBtn = $(modal).find('.panel-footer .btn-back');
+		var saveBtn = $(modal).find('.panel-footer .btn-save');
+		var newBtn = $('#addServer');
+
+		$(newBtn).on('click', function() {
+			$(modal).show();
+		});
+
+		$(closeBtn).on('click', function() {
+			$(actionForm).reset();
+			$(modal).hide();
+		});
+
+		$(backBtn).on('click', function() {
+			$(actionForm).reset();
+			$(modal).hide();
+		});
+
+		$(saveBtn).on('click', function() {
+			$(actionForm).submit();
+		});
+
+		$(actionForm).submit(function() {
+			if ($(actionForm).valid()) {
+				var formObject = $(this).serializeObject();
+				var url = subscriptionType === IPTV ? '/iptv' : '/sharing';
+				url += '/subscriptions';
+
+				$.ajax({
+					type : 'POST',
+					url : url,
+					data : formObject,
+					success : function(result) {
+						$(modal).hide();
+						toastr['success'](result);
+						$('#toast-container .toast-success').show();
+						timer = setTimeout(function() {
+							location.reload();
+						}, 1000);
+					},
+					error : function(error) {
+						toastr['error']("error occured ! ");
+						$('#toast-container .toast-error').show();
+					}
+				});
+			}
+			return false;
+		});
+
+	}
+
+	
 });
--- src/main/resources/static/js/subscriptions.js
+++ src/main/resources/static/js/subscriptions.js
@@ -1,78 +1,78 @@
 $(function() {
 
-    const
-    IPTV = 'IPTV', SHARING = 'SHARING';
+	const IPTV = 'IPTV', SHARING = 'SHARING';
 
-    var subscriptionsContainer = $('.subscriptions-container');
-    var subscriptionType = $(subscriptionsContainer).data('subscription-type');
+	var subscriptionsContainer = $('.subscriptions-container');
+	var subscriptionType = $(subscriptionsContainer).data('subscription-type');
 
-    initSubscriptionsDataTables();
-    initModal();
+	initSubscriptionsDataTables();
+	initModal();
 
-    function initSubscriptionsDataTables() {
-	var subscriptionsDataTable = $('#subscriptions-datatables').DataTable(dataTablesConfig);
-	return subscriptionsDataTable.rows().count();
-    }
+	function initSubscriptionsDataTables() {
+		var subscriptionsDataTable = $('#subscriptions-datatables').DataTable(
+				dataTablesConfig);
+		return subscriptionsDataTable.rows().count();
+	}
 
-    function initModal() {
+	function initModal() {
 
-	var modal = $('#page-wrapper').find('#subscriptionModal');
-	var actionForm = $(modal).find('form');
-	var closeBtn = $(modal).find('.close');
-	var backBtn = $(modal).find('.panel-footer .btn-back');
-	var saveBtn = $(modal).find('.panel-footer .btn-save');
-	var newBtn = $('#newSubscription');
+		var modal = $('#page-wrapper').find('#subscriptionModal');
+		var actionForm = $(modal).find('form');
+		var closeBtn = $(modal).find('.close');
+		var backBtn = $(modal).find('.panel-footer .btn-back');
+		var saveBtn = $(modal).find('.panel-footer .btn-save');
+		var newBtn = $('#newSubscription');
 
-	$(newBtn).on('click', function() {
-	    $(modal).show();
-	});
-
-	$(closeBtn).on('click', function() {
-	    $(actionForm).reset();
-	    $(modal).hide();
-	});
-
-	$(backBtn).on('click', function() {
-	    $(actionForm).reset();
-	    $(modal).hide();
-	});
-
-	$(saveBtn).on('click', function() {
-	    $(actionForm).submit();
-	});
-
-	$(actionForm).submit(function() {
-	    if ($(actionForm).valid()) {
-		var formObject = $(this).serializeObject();
-		var url = subscriptionType === IPTV ? '/iptv' : '/sharing';
-		url += '/subscriptions';
-
-		$.ajax({
-		    type : 'POST',
-		    url : url,
-		    data : formObject,
-		    success : function(result) {
-			$(modal).hide();
-			toastr['success'](result);
-			$('#toast-container .toast-success').show();
-			timer = setTimeout(function() {
-			    location.reload();
-			}, 1000);
-		    },
-		    error : function(error) {
-			toastr['error']("error occured ! ");
-			$('#toast-container .toast-error').show();
-		    }
+		$(newBtn).on('click', function() {
+			$(modal).show();
 		});
-	    }
-	    return false;
-	});
 
-	$(window).on('click', function(event) {
-	    if (event.target == modal) {
-		$(modal).hide();
-	    }
-	});
-    }
+		$(closeBtn).on('click', function() {
+			$(actionForm).reset();
+			$(modal).hide();
+		});
+
+		$(backBtn).on('click', function() {
+			$(actionForm).reset();
+			$(modal).hide();
+		});
+
+		$(saveBtn).on('click', function() {
+			$(actionForm).submit();
+		});
+
+		$(actionForm).submit(function() {
+			if ($(actionForm).valid()) {
+				var formObject = $(this).serializeObject();
+				var url = subscriptionType === IPTV ? '/iptv' : '/sharing';
+				url += '/subscriptions';
+
+				$.ajax({
+					type : 'POST',
+					url : url,
+					data : formObject,
+					success : function(result) {
+						$(modal).hide();
+						toastr['success'](result);
+						$('#toast-container .toast-success').show();
+						timer = setTimeout(function() {
+							location.reload();
+						}, 1000);
+					},
+					error : function(error) {
+						toastr['error']("error occured ! ");
+						$('#toast-container .toast-error').show();
+					}
+				});
+			}
+			return false;
+		});
+
+		$(window).on('click', function(event) {
+			if (event.target == modal) {
+				$(modal).hide();
+			}
+		});
+	}
 
 });
